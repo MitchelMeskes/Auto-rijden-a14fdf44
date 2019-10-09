1fa18d7323bf3f2cf18abb7ac46acbc969c4c428
Mitchel Meskes <2115621@talnet.nl>
2019-10-09 09:44:07 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hallo Gebruiker, Wat is je leeftijd?\n";
+$leeftijd = readline();
+$minleef =15.5;
+
+if ($minleef<$leeftijd)
+    echo "Je mag beginnen met rijlessen!";
+else
+    echo "Helaas, je mag nog niet beginnen met rijlessen";
+
+
+
+
+
+
+
+?>
